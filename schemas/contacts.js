16f1bf7aecd24fa640664409e591e0ae60cad221
LiudmylaBolotova <liudmylabolotova@gmail.com>
2023-08-07 00:50:50 +0300

    fixed

--- schemas/contacts.js
+++ schemas/contacts.js
@@ -1,9 +1,11 @@
 const Joi = require("joi");
 
 const addSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().required(),
-  phone: Joi.string().required(),
+  name: Joi.string().min(3).max(25).required(),
+  email: Joi.string().min(5).max(255).required().email(),
+  phone: Joi.string()
+    .regex(/^[0-9]{12}$/)
+    .required(),
 });
 
 module.exports = {
